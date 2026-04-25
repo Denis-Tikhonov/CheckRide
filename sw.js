779abf346fedab53f814018dc0d37c5096ee9f7c
Denis-Tikhonov <107187923+Denis-Tikhonov@users.noreply.github.com>
2026-04-25 13:28:00 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,33 @@
+const CACHE_NAME = "checkride-v6-enterprise";
+const ASSETS = [
+    "./",
+    "./index.html",
+    "./app.js",
+    "./style.css",
+    "./data.json",
+    "./data_ffs.json",
+    "https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"
+];
 
-const CACHE="checkride-v5"
+// Установка: кешируем все файлы
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
 
-self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open(CACHE).then(c=>c.addAll([
-"/",
-"/index.html",
-"/app.js",
-"/style.css",
-"/data.json",
-  "/data_line.json",
-  "/data_ffs.json"
-]))
-)
-})
+// Активация: чистим старый кеш
+self.addEventListener("activate", (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+        })
+    );
+});
 
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request).then(r=>r||fetch(e.request))
-)
-})
+// Запрос: сначала смотрим в кеш, потом в сеть
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => res || fetch(e.request))
+    );
+});
