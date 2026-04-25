9019676344bb3f713e3a049d8c170406ee4d3fd4
Denis-Tikhonov <107187923+Denis-Tikhonov@users.noreply.github.com>
2026-04-25 11:33:36 +0500

    Add files via upload
    start

---
+++ sw.js
@@ -0,0 +1,24 @@
+
+const CACHE="checkride-v3";
+
+self.addEventListener("install",e=>{
+e.waitUntil(
+caches.open(CACHE).then(cache=>{
+return cache.addAll([
+"/",
+"/index.html",
+"/script.js",
+"/style.css",
+"/data.json"
+])
+})
+);
+});
+
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request).then(r=>{
+return r || fetch(e.request);
+})
+);
+});
